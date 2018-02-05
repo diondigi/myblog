5fbf5c63c7787447d2e5291bf8a7cabbbca34f89
diondigi <417144633@qq.com>
2018-02-05 22:35:29 +0800

    update paginator action

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,130 @@ class IndexView(ListView):
     context_object_name = 'post_list'
     paginate_by = 2
 
+    def get_context_data(self, **kwargs):
+        """
+        在视图函数中将模板变量传递给模板是通过给 render 函数的 context 参数传递一个字典实现的，
+        例如 render(request, 'blog/index.html', context={'post_list': post_list})，
+        这里传递了一个 {'post_list': post_list} 字典给模板。
+        在类视图中，这个需要传递的模板变量字典是通过 get_context_data 获得的，
+        所以我们复写该方法，以便我们能够自己再插入一些我们自定义的模板变量进去。
+        """
+
+        # 首先获得父类生成的传递给模板的字典。
+        context = super().get_context_data(**kwargs)
+
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        # 调用自己写的 pagination_data 方法获得显示分页导航条需要的数据，见下方。
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+
+        # 将分页导航条的模板变量更新到 context 中，注意 pagination_data 方法返回的也是一个字典。
+        context.update(pagination_data)
+
+        # 将更新后的 context 返回，以便 ListView 使用这个字典中的模板变量去渲染模板。
+        # 注意此时 context 字典中已有了显示分页导航条所需的数据。
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages # 获取分页后的总页数
+        page_range = paginator.page_range
+        if page_number == 1:
+            right = page_range[ page_number : page_number + 2]
+            # 如果最右边的页码号比最后一页的页码号减去 1 还要小，
+            # 说明最右边的页码号和最后一页的页码号之间还有其它页码，因此需要显示省略号，通过 right_has_more 来指示。
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            # 如果最右边的页码号比最后一页的页码号小，说明当前页右边的连续页码号中不包含最后一页的页码
+            # 所以需要显示最后一页的页码号，通过 last 来指示
+            if right[-1] > total_pages:
+                last = True
+
+
+        elif page_number == total_pages:
+
+            # 如果用户请求的是最后一页的数据，那么当前页右边就不需要数据，因此 right=[]（已默认为空），
+
+            # 此时只要获取当前页左边的连续页码号。
+
+            # 比如分页页码列表是 [1, 2, 3, 4]，那么获取的就是 left = [2, 3]
+
+            # 这里只获取了当前页码后连续两个页码，你可以更改这个数字以获取更多页码。
+
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+
+            # 如果最左边的页码号比第 2 页页码号还大，
+
+            # 说明最左边的页码号和第 1 页的页码号之间还有其它页码，因此需要显示省略号，通过 left_has_more 来指示。
+
+            if left[0] > 2:
+                left_has_more = True
+
+            # 如果最左边的页码号比第 1 页的页码号大，说明当前页左边的连续页码号中不包含第一页的页码，
+
+            # 所以需要显示第一页的页码号，通过 first 来指示
+
+            if left[0] > 1:
+                first = True
+
+        else:
+
+            # 用户请求的既不是最后一页，也不是第 1 页，则需要获取当前页左右两边的连续页码号，
+
+            # 这里只获取了当前页码前后连续两个页码，你可以更改这个数字以获取更多页码。
+
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+
+            right = page_range[page_number:page_number + 2]
+
+            # 是否需要显示最后一页和最后一页前的省略号
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                last = True
+
+            # 是否需要显示第 1 页和第 1 页后的省略号
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+
+        data = {
+
+            'left': left,
+
+            'right': right,
+
+            'left_has_more': left_has_more,
+
+            'right_has_more': right_has_more,
+
+            'first': first,
+
+            'last': last,
+
+        }
+
+        return data
+
+
+
+
+
 class PostDetailView(DetailView):
     # 这些属性的含义和 ListView 是一样的
     model = Post
@@ -71,10 +195,10 @@ class ArchivesView(ListView):
                                                                )
 
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate)
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 class CategoryView(ListView):
